f34eb31c9ba2100eff7857530297d1ad86a04328
simon.norton <simon@nitritex.com>
2017-01-03 16:43:29 +0000

    got names into the table

--- src/app/approve/approve.component.ts
+++ src/app/approve/approve.component.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { HolidayService } from '../shared/holiday.service';
+import { UserListService } from '../shared/user-list.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/Observable/combineLatest';
+
+/*
+instance method
+import 'rxjs/add/operator/merge';
+const a = Observable.of(1);
+a.merge(Observable.of(2)).subscribe(...);
+
+static method
+import 'rxjs/add/observable/merge';
+Observable.merge(Observable.of(1), Observable.of(2)).subscribe(...);
+*/
 
 @Component({
   selector: 'app-approve',
@@ -9,9 +24,11 @@ import { HolidayService } from '../shared/holiday.service';
 })
 export class ApproveComponent implements OnInit {
 
-  constructor(public HolidayService:HolidayService) { }
+  constructor(public HolidayService:HolidayService,public UserListService:UserListService) { }
 
   holidays$;
+  users$;
+  mergeHolidays$;
   bookings; //array
 
   //change booking to approve 
@@ -26,15 +43,19 @@ export class ApproveComponent implements OnInit {
   }
 
   ngOnInit() {
-    // build list of bookings. Will have to attach name.
+    // build list of bookings. 
+    // Will have to attach name, merge streams?
     this.holidays$ = this.HolidayService.getAllHolidays();
+    this.users$ = this.UserListService.getUsers();
+
+    this.mergeHolidays$ = this.HolidayService.getAllHolidaysAndUsers();
 
     //TEST
-    this.bookings = [
-      {key:'-KYKAvI3Bwzly1XBwnxv', name:'Simon Norton',date:'21/05/2016',type:'Sick',slot:'Morning',status:'approved',comments:'Time off for visit'},
-      {key:'-KYTlFBUn-zEOJW6KFOn', name:'Simon Norton',date:'22/05/2016',type:'Sick',slot:'Full',status:'approved',comments:'blah'},
-      {name:'Chris Wilson',date:'21/05/2016',type:'Leave',slot:'Full',status:'rejected',comments:'blah'}
-    ]
+    // this.bookings = [
+    //   {key:'-KYKAvI3Bwzly1XBwnxv', name:'Simon Norton',date:'21/05/2016',type:'Sick',slot:'Morning',status:'approved',comments:'Time off for visit'},
+    //   {key:'-KYTlFBUn-zEOJW6KFOn', name:'Simon Norton',date:'22/05/2016',type:'Sick',slot:'Full',status:'approved',comments:'blah'},
+    //   {name:'Chris Wilson',date:'21/05/2016',type:'Leave',slot:'Full',status:'rejected',comments:'blah'}
+    // ]
   }
 
 }
