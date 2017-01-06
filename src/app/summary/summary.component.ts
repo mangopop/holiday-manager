e8599d02e6a8922bd85dadf6c841ce2e6e5e568a
simon.norton <simon@nitritex.com>
2017-01-06 16:39:19 +0000

    using team filter

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HolidayService } from '../shared/holiday.service';
+import { UserListService } from '../shared/user-list.service';
 
 @Component({
   selector: 'app-summary',
@@ -8,7 +9,7 @@ import { HolidayService } from '../shared/holiday.service';
 })
 export class SummaryComponent implements OnInit {
 
-  constructor(public HolidayService: HolidayService) { }
+  constructor(public HolidayService: HolidayService, public UserListService: UserListService) { }
 
   summary$;
   results: string[] = [];
@@ -17,31 +18,35 @@ export class SummaryComponent implements OnInit {
   ngOnInit() {
     // filter out people with unbooked hols
     //loop through each hol, add up days taken - basic + service + xhol 
-
-    this.HolidayService.getAllUserWithHolidays().mergeMap(data => {
-      console.log(data);
-
-      return data.map(item => {
-        return item.holiday.map(hol => {
-          var holleft = 20 - hol.reduce((prev, curr) => prev + curr.daysTaken, 0)
-          if (holleft > 0) {
-            return {
-              firstname: item.firstname,
-              surname: item.surname,
-              hol: holleft
+    //might want to kill this sub as changing user was fucking things up
+    this.UserListService.getUserByEmail().mergeMap(user => {
+      return this.HolidayService.getAllUserWithHolidays().mergeMap(data => {
+        // console.log(data); //array 
+        // console.log(user); //array
+        return data.filter(team => team.team === user[0].team).map(item => {
+          // console.log(item); //object          
+          return item.holiday.map(hol => {
+            var holleft = 20 - hol.reduce((prev, curr) => prev + curr.daysTaken, 0)
+            if (holleft > 0) {
+              return {
+                firstname: item.firstname,
+                surname: item.surname,
+                hol: holleft
+              }
             }
-          }
 
-        })
-      });
+          })
+        });
 
+      })
     }).subscribe(item => {
-      this.summary$ = item;
-      item.subscribe(test => {
-        console.log(test);        
-        this.results.push(test);
-      });
-    });
+        this.summary$ = item;
+        item.subscribe(test => {
+          this.results.push(test);
+        });
+      });;
+
+
 
 
   }
