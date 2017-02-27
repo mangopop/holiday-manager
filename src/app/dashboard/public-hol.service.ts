883cd47554d8125c7d9053eb8b5263d9d10bebb8
simon.norton <simon@nitritex.com>
2017-02-27 12:26:53 +0000

    for some inexplicable reason the jsonp has stopped working on this service, prob because of header type. Also filter doesn't work on req.json() anymore

--- src/app/dashboard/public-hol.service.ts
+++ src/app/dashboard/public-hol.service.ts
@@ -25,18 +25,36 @@ export class PublicHolService {
   }
 
   // this is returning a block of json?
-  getBankHoliday(year): Observable<any> {
-    let params = new URLSearchParams();
-    // let year = new Date().getFullYear();
-    params.set('callback', 'JSONP_CALLBACK');
+  // getBankHolidayNonSecure(year): Observable<any> {
+  //   let params = new URLSearchParams();
+  //   // let year = new Date().getFullYear();
+  //   params.set('callback', 'JSONP_CALLBACK');
 
-    return this.jsonp.get(
-      'http://kayaposoft.com/enrico/json/v1.0/?action=getPublicHolidaysForYear&year='+year+'&country=eng&jsonp=JSONP_CALLBACK')
+  //   return this.jsonp.get(
+  //     'http://kayaposoft.com/enrico/json/v1.0/?action=getPublicHolidaysForYear&year='+year+'&country=eng&jsonp=JSONP_CALLBACK')
+  //     .map((res: Response) => {
+  //       // return res.json().filter(data => data.id === 1) || {}; for extracting 1 user from list
+  //       return res.json() || {};
+  //     })
+  //     // .startWith({ date: 'Loading...' })
+  //     .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  // }
+
+    // this is returning a block of json?
+  getBankHoliday(): Observable<any> {
+    // let params = new URLSearchParams();
+    // let year = new Date().getFullYear();
+    // params.set('jsonp', 'JSONP_CALLBACK');
+
+    return this.http.get(
+      '/json/hols.json')
       .map((res: Response) => {
-        // return res.json().filter(data => data.id === 1) || {}; for extracting 1 user from list
-        return res.json() || {};
+        console.log(res.json().filter(data => data.division === 'england-and-wales'));
+        return res.json().filter(data => data.division === 'england-and-wales') || {};
+        // return res.json() || {};
       })
       // .startWith({ date: 'Loading...' })
-      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+      .catch((error: any) => Observable.throw(error || 'Server error'));
+      // .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
   }
 }
